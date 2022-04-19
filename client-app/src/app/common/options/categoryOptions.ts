3520e2665f738dab7a89b4804a0df8e65c5f8602
Jared Shane <jared.shane@americaneagle.com>
2022-04-18 20:24:22 -0400

    End of section 11

---
+++ client-app/src/app/common/options/categoryOptions.ts
@@ -0,0 +1,8 @@
+export const categoryOptions = [
+    {text: 'Drinks', value: 'drinks'},
+    {text: 'Culture', value: 'culture'},
+    {text: 'Film', value: 'film'},
+    {text: 'Food', value: 'Ffod'},
+    {text: 'Music', value: 'Mmsic'},
+    {text: 'Travel', value: 'travel'},
+]
